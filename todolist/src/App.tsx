e13d056ecd60d490aa6502e2f21801dc0fcd0ec7
Roman <romantut1988@mail.ru>
2021-11-16 00:19:24 +0300

    changed folder name

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import TodoList from './TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
 }
 
+function App() {
+    const todoListTitle: string = 'What to learn'
+
+      const [tasks, setTasks] = useState<Array<TaskType>>( [
+        {id: 1, title: 'HTML', isDone: true},
+        {id: 2, title: 'CSS', isDone: true},
+        {id: 3, title: 'REACT', isDone: false},
+    ])
+
+    const [filter, setFilter] = useState<'all'|'active'|'complited'>('all')
+
+    const changeFilter = (filter: 'all'|'active'|'complited') => {
+        setFilter(filter)
+    }
+
+    const removeTask = (taskID: number) => {
+        setTasks(tasks.filter(task=> task.id !== taskID))
+    }
+
+    let tasksForRender = tasks
+    if (filter === 'active') {
+        tasksForRender = tasks.filter(t=>t.isDone === false)
+    }
+
+    if (filter === 'complited') {
+        tasksForRender = tasks.filter(t=>t.isDone === true)
+    }
+
+    return (
+        <div className='App'>
+            <TodoList
+                title={todoListTitle}
+                tasks={tasksForRender}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+            />
+        </div>
+    );
+}
 export default App;
