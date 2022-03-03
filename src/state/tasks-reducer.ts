bf12cc7490413af90edb6bbac09bb2a98b7a68dc
romantut1988 <romantut1988@mail.ru>
2022-03-03 10:53:37 +0300

    the useState hook uses useReducer

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,7 +1,6 @@
 import {FilterValuesType, TasksStateType, TodolistType} from '../App';
 import {v1} from 'uuid';
 import {Simulate} from "react-dom/test-utils";
-import copy = Simulate.copy;
 
 export type RemoveTaskActionType = {
     type: 'REMOVE-TASK'
@@ -13,15 +12,21 @@ export type AddTaskActionType = {
     title: string
     todolistId: string
 }
+export type ChangeTaskStatusActionType = {
+    type: 'CHANGE-TASK-STATUS'
+    taskId: string
+    todolistId: string
+    isDone: boolean
+}
 
-type ActionsType = RemoveTaskActionType | AddTaskActionType;
+type ActionsType = RemoveTaskActionType | AddTaskActionType | ChangeTaskStatusActionType;
 
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
     switch (action.type) {
         case "REMOVE-TASK": {
             const stateCopy = {...state};
             const tasks = state[action.todolistId];
-            const filteredTasks = tasks.filter(t => t.id ! == action.taskId)
+            const filteredTasks = tasks.filter(t => t.id !== action.taskId)
             stateCopy[action.todolistId] = filteredTasks;
             return stateCopy;
         }
@@ -30,8 +35,8 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksS
             const tasks = stateCopy[action.todolistId];
             const newTask = {id: v1(), title: action.title, isDone: false}
             const newTasks = [newTask, ...tasks];
-            stateCopy[action.todolistId] = newTasks
-            return stateCopy
+            stateCopy[action.todolistId] = newTasks;
+            return stateCopy;
         }
         default:
             throw new Error("I don't understand this type")
@@ -43,5 +48,8 @@ export const removeTaskAC = (taskId: string, todolistId: string): RemoveTaskActi
 }
 export const addTaskAC = (title: string, todolistId: string): AddTaskActionType => {
     return {type: 'ADD-TASK', title, todolistId}
+}
+export const changeTaskStatusAC = (taskId: string, isDone: boolean, todolistId: string): ChangeTaskStatusActionType => {
+    return {type: 'CHANGE-TASK-STATUS', isDone, todolistId, taskId}
 }
 
