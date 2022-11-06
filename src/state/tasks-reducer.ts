d24f52b94cc2659af47507df03f5e63397a2c2cd
romantut1988 <romantut1988@mail.ru>
2022-11-07 01:15:39 +0300

    reducer for tasks, unit test

---
+++ src/state/tasks-reducer.ts
@@ -0,0 +1,34 @@
+import {TasksStateType} from '../App';
+import {v1} from "uuid";
+
+export type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
+export type AddTaskActionType = ReturnType<typeof addTaskAC>
+
+type ActionsType = RemoveTaskActionType | AddTaskActionType
+
+
+export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
+    switch (action.type) {
+        case 'REMOVE-TASK':
+            return {
+                ...state,
+                [action.todolistId]: state[action.todolistId].filter(task => task.id !== action.taskId)
+            }
+        case 'ADD-TASK':
+            return {
+                ...state,
+                [action.todolistId]: [{id: v1(), title: action.title, isDone: false}, ...state[action.todolistId]]
+            }
+        default:
+            return state
+    }
+}
+
+export const removeTaskAC = (taskId: string, todolistId: string) => {
+    return {type: 'REMOVE-TASK', taskId, todolistId} as const
+}
+export const addTaskAC = (title: string, todolistId: string) => {
+    return {type: 'ADD-TASK', title, todolistId} as const
+}
+
+
