bb41040e5a6cad12e6749790f3dfde76132a9b8f
romantut1988 <romantut1988@mail.ru>
2022-11-26 00:34:29 +0300

    case to add empty

--- src/App.tsx
+++ src/App.tsx
@@ -20,8 +20,7 @@ import {
     TodolistDomainType
 } from './state/todolists-reducer'
 import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC} from './state/tasks-reducer';
-import { useSelector} from 'react-redux';
-import {AppRootStateType, useAppDispatch} from './state/store';
+import {useAppDispatch, useAppSelector} from './state/store';
 import {TaskStatuses, TaskType} from './api/todolists-api'
 
 
@@ -32,8 +31,8 @@ export type TasksStateType = {
 
 function App() {
 
-    const todolists = useSelector<AppRootStateType, Array<TodolistDomainType>>(state => state.todolists)
-    const tasks = useSelector<AppRootStateType, TasksStateType>(state => state.tasks)
+    const todolists = useAppSelector<Array<TodolistDomainType>>(state => state.todolists)
+    const tasks = useAppSelector<TasksStateType>(state => state.tasks)
     const dispatch = useAppDispatch();
 
     const removeTask = useCallback(function (id: string, todolistId: string) {
@@ -77,7 +76,7 @@ function App() {
     }, [dispatch]);
 
     useEffect(() => {
-       dispatch(getTodoListsThunk)
+        dispatch(getTodoListsThunk)
     }, [])
 
     return (
--- src/state/store.ts
+++ src/state/store.ts
@@ -2,7 +2,7 @@ import {tasksReducer} from './tasks-reducer';
 import {todolistsReducer} from './todolists-reducer';
 import {AnyAction, applyMiddleware, combineReducers, createStore} from 'redux';
 import thunkMiddleware, {ThunkDispatch} from 'redux-thunk';
-import {useDispatch} from "react-redux";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -17,6 +17,7 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 export type ThunkAppDispatchType = ThunkDispatch<AppRootStateType, any, AnyAction>
 
 export const useAppDispatch = () => useDispatch<ThunkAppDispatchType>()
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
