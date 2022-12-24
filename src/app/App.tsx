65c2694ba47cf00830c13fcc321ae01ae23b111a
romantut1988 <romantut1988@mail.ru>
2022-12-25 02:42:36 +0300

    create a form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,6 +13,7 @@ import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
 import { Menu } from '@mui/icons-material';
 import { ErrorSnackbar } from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Login} from "../features/Login/Login";
 
 type PropsType = {
     demo?: boolean
@@ -37,6 +38,7 @@ function App({demo = false}: PropsType) {
             </AppBar>
             <Container fixed>
                 <TodolistsList demo={demo}/>
+                <Login />
             </Container>
         </div>
     )
