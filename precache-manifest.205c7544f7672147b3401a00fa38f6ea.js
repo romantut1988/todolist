a91287e701e76c15ccea0b86ce2bdbdf12ee2f02
Roman <romantut1988@mail.ru>
2023-04-16 18:11:29 +0300

    Updates

---
+++ precache-manifest.205c7544f7672147b3401a00fa38f6ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66cbe87aeaa6e29bc875291978ce8387",
+    "url": "/todolist-incubator/index.html"
+  },
+  {
+    "revision": "5e19a2e4625cbbfea6ca",
+    "url": "/todolist-incubator/static/css/main.6da83bb5.chunk.css"
+  },
+  {
+    "revision": "b853e9c1599992e97f8f",
+    "url": "/todolist-incubator/static/js/2.1f608c14.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist-incubator/static/js/2.1f608c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e19a2e4625cbbfea6ca",
+    "url": "/todolist-incubator/static/js/main.69fda8e8.chunk.js"
+  },
+  {
+    "revision": "562305b3e27188c2e3ec",
+    "url": "/todolist-incubator/static/js/runtime-main.8b98fea1.js"
+  }
+]);
